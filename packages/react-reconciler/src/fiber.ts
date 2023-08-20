538573ae2b85e00811124047a09637248883ca0b
New <mynewgh@163.com>
2023-08-20 23:38:40 +0800

    feat: 新增打包

---
+++ packages/react-reconciler/src/fiber.ts
@@ -0,0 +1,30 @@
+import { Props, Key, Ref } from 'shared/ReactTypes';
+import { WorkTag } from './workTags';
+export class FiberNode {
+	tag: WorkTag;
+	type: any;
+	key: Key;
+	stateNode: any;
+	pendingProps: Props | null;
+	memoizedProps: Props | null;
+	return: FiberNode | null;
+	sibling: FiberNode | null;
+	child: FiberNode | null;
+	index: number;
+	ref: Ref;
+	constructor(tag: WorkTag, pendingProps: Props, key: Key) {
+		this.tag = tag;
+		this.key = key;
+		this.stateNode = null; // div
+		this.type = null; // funComponent ()=>{}
+		// 构成树状结构
+		this.return = null; // 指向父fiberNode
+		this.sibling = null; // 指向兄弟fiberNode
+		this.child = null; // 指向子fiberNode
+		this.index = 0;
+		this.ref = null;
+		// 工作单元
+		this.pendingProps = pendingProps;
+		this.memoizedProps = null;
+	}
+}
